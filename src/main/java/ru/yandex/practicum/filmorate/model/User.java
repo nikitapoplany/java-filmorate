8e8a1edf8e51d0381390b697b205cedb5814b14b
h0ttab <myemail@example.com>
2025-07-29 11:19:21 +0300

    feat(data model): Add @Builder to User model

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -3,9 +3,11 @@ package ru.yandex.practicum.filmorate.model;
 import java.time.LocalDate;
 
 import jakarta.validation.constraints.*;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class User {
     private Integer id;
 

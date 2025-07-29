de58cda78131a0a162bcdf46e52d41a17e19c493
h0ttab <myemail@example.com>
2025-07-29 11:52:40 +0300

    feat(test): Add validation tests for User data model

--- src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerTest.java
@@ -1,5 +1,7 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import java.time.LocalDate;
+
 import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,15 +16,96 @@ public class UserControllerTest {
     private WebTestClient webTestClient;
 
     @Test
-    void shouldFailOnInvalidInput_createUserTest() {
-        User user = new User();
+    void shouldFailOnInvalidInput_createUserTest_emptyUser() {
         webTestClient.post()
-                .uri("/films")
+                .uri("/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(User.builder().build())
+                .exchange()
+                .expectStatus()
+                .is4xxClientError();
+    }
+
+    @Test
+    void shouldFailOnInvalidInput_createUserTest_invalidEmail() {
+        User user = User.builder()
+                .email("abc.invalid")
+                .login("Login")
+                .name("Alex")
+                .birthday(LocalDate.of(1990, 12, 10))
+                .build();
+        webTestClient.post()
+                .uri("/users")
                 .contentType(MediaType.APPLICATION_JSON)
                 .bodyValue(user)
                 .exchange()
                 .expectStatus()
                 .is4xxClientError();
+    }
+
+    @Test
+    void shouldFailOnInvalidInput_createUserTest_blankNullLogin() {
+        User userBlankLogin = User.builder()
+                .email("abc.valid@email.com")
+                .login("")
+                .name("Alex")
+                .birthday(LocalDate.of(1990, 12, 10))
+                .build();
+        webTestClient.post()
+                .uri("/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(userBlankLogin)
+                .exchange()
+                .expectStatus()
+                .is4xxClientError();
+
+        User userNullLogin = User.builder()
+                .email("abc.valid@email.com")
+                .login(null)
+                .name("Alex")
+                .birthday(LocalDate.of(1990, 12, 10))
+                .build();
+        webTestClient.post()
+                .uri("/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(userBlankLogin)
+                .exchange()
+                .expectStatus()
+                .is4xxClientError();
+    }
+
+    @Test
+    void shouldFailOnInvalidInput_createUserTest_invalidBirthDate() {
+        User user = User.builder()
+                .email("abc.valid@email.com")
+                .login("Login")
+                .name("Alex")
+                .birthday(LocalDate.of(9999, 12, 10))
+                .build();
+        webTestClient.post()
+                .uri("/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(User.builder().build())
+                .exchange()
+                .expectStatus()
+                .is4xxClientError();
+    }
+
+    @Test
+    void shouldNotFailOnValidInput_createUserTest() {
+        User user = User.builder()
+                .email("abc.valid@email.com")
+                .login("Login")
+                .name("Alex")
+                .birthday(LocalDate.of(1990, 12, 10))
+                .build();
+        webTestClient.post()
+                .uri("/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(user)
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful();
     }
 }
 

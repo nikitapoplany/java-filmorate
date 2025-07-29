45d9fb6eb77370b123d8488b0d2e0381fafcbddb
h0ttab <myemail@example.com>
2025-07-29 14:03:12 +0300

    feat(test): Add validation tests for Film data model

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -3,10 +3,12 @@ package ru.yandex.practicum.filmorate.model;
 import java.time.LocalDate;
 
 import jakarta.validation.constraints.*;
+import lombok.Builder;
 import lombok.Data;
 import org.hibernate.validator.constraints.Length;
 
 @Data
+@Builder
 public class Film {
     private Integer id;
 
@@ -14,11 +16,14 @@ public class Film {
     @NotBlank
     private String name;
 
+    @NotNull
     @Length(max = 200)
     private String description;
 
+    @NotNull
     private LocalDate releaseDate;
 
+    @NotNull
     @Positive
     private Integer duration;
 }
--- src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
@@ -1,5 +1,7 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import java.time.LocalDate;
+
 import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,12 +12,13 @@ import ru.yandex.practicum.filmorate.model.Film;
 
 @ControllerTest
 public class FilmControllerTest {
+
     @Autowired
     private WebTestClient webTestClient;
 
     @Test
-    void shouldFailOnInvalidInput_createFilmTest() {
-        Film film = new Film();
+    void shouldFailOnInvalidInput_createFilmTest_emptyFilm() {
+        Film film = Film.builder().build();
         webTestClient.post()
                 .uri("/films")
                 .contentType(MediaType.APPLICATION_JSON)
@@ -23,6 +26,81 @@ public class FilmControllerTest {
                 .exchange()
                 .expectStatus()
                 .is4xxClientError();
+    }
+
+    @Test
+    void shouldFailOnInvalidInput_createFilmTest_invalidReleaseDate() {
+        Film film = Film.builder()
+                .name("Title")
+                .description("About")
+                .releaseDate(LocalDate.of(1800, 1, 1))
+                .duration(100)
+                .build();
+
+        webTestClient.post()
+                .uri("/films")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(film)
+                .exchange()
+                .expectStatus()
+                .is4xxClientError();
+    }
+
+    @Test
+    void shouldFailOnInvalidInput_createFilmTest_tooLongDescription() {
+        String descriptionLength250 = """
+                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, 
+                sed diam nonumy eirmod tempor invidunt ut labore et dolore 
+                magna aliquyam erat, sed diam voluptua. At vero eos et accusam 
+                et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea""";
+
+        Film film = Film.builder()
+                .name("Title")
+                .description(descriptionLength250)
+                .releaseDate(LocalDate.of(1990, 5, 6))
+                .duration(200)
+                .build();
+
+        webTestClient.post()
+                .uri("/films")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(film)
+                .exchange()
+                .expectStatus()
+                .is4xxClientError();
+    }
+
+    @Test
+    void shouldFailOnInvalidInput_createFilmTest_missingFields() {
+        Film film = Film.builder()
+                .name("Title")
+                .duration(200)
+                .build();
+
+        webTestClient.post()
+                .uri("/films")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(film)
+                .exchange()
+                .expectStatus()
+                .is4xxClientError();
+    }
+    @Test
+    void shouldNotFailOnValidInput_createUserTest_validRequest() {
+        Film film = Film.builder()
+                .name("Title")
+                .description("About")
+                .releaseDate(LocalDate.of(2010, 1, 1))
+                .duration(100)
+                .build();
+
+        webTestClient.post()
+                .uri("/films")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(film)
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful();
     }
 }
 

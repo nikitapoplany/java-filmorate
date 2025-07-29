f63745cbb29585f7ed14af2f859defaedb6d22bd
h0ttab <myemail@example.com>
2025-07-29 11:51:23 +0300

    feat(exception): Make input validation error message more informative

--- src/main/java/ru/yandex/practicum/filmorate/handler/RestExceptionHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/handler/RestExceptionHandler.java
@@ -2,7 +2,7 @@ package ru.yandex.practicum.filmorate.handler;
 
 import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -11,11 +11,9 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class RestExceptionHandler {
 
     @ExceptionHandler(ConstraintViolationException.class)
-    public ResponseEntity<Void> handleException(ConstraintViolationException e) {
+    public ResponseEntity<?> handleException(ConstraintViolationException e) {
         log.error(e.getMessage(), e);
-        // ResponseEntity.status(HttpStatus.BAD_REQUEST).body()
-        return null;
+        return new ResponseEntity<>(String.format("An error occurred while handling request:%n %s",
+                e.getMessage()), HttpStatus.BAD_REQUEST);
     }
 }
-
-// org.springframework.web.method.annotation.HandlerMethodValidationException: 400 BAD_REQUEST "Validation failure"

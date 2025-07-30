62049dbe891451f339bda9f18ef9be4024898b20
h0ttab <myemail@example.com>
2025-07-30 18:05:15 +0300

    feat(dto): Create dto -> entity mappers

---
+++ src/main/java/ru/yandex/practicum/filmorate/dto/film/FilmMapper.java
@@ -0,0 +1,37 @@
+package ru.yandex.practicum.filmorate.dto.film;
+
+import jakarta.validation.ValidationException;
+import ru.yandex.practicum.filmorate.exception.LoggedException;
+import ru.yandex.practicum.filmorate.film.Film;
+import ru.yandex.practicum.filmorate.film.FilmService;
+
+import static ru.yandex.practicum.filmorate.util.Validators.isValidFilmReleaseDate;
+
+public class FilmMapper {
+
+    public static Film toEntity(FilmCreateDto filmCreateDto) {
+        if (!isValidFilmReleaseDate(filmCreateDto.getReleaseDate())) {
+            LoggedException.throwNew(
+                    new ValidationException(String.format("Дата создания фильма не может быть ранее 28 декабря 1895 г."
+                                    + " Некорректная дата - %s",
+                            filmCreateDto.getReleaseDate())), FilmService.class);
+        }
+
+        return Film.builder()
+                .name(filmCreateDto.getName())
+                .description(filmCreateDto.getDescription())
+                .duration(filmCreateDto.getDuration())
+                .releaseDate(filmCreateDto.getReleaseDate())
+                .build();
+    }
+
+    public static Film toEntity(FilmUpdateDto filmUpdateDto) {
+        return Film.builder()
+                .id(filmUpdateDto.getId())
+                .name(filmUpdateDto.getName().orElse(null))
+                .duration(filmUpdateDto.getDuration().orElse(null))
+                .description(filmUpdateDto.getDescription().orElse(null))
+                .releaseDate(filmUpdateDto.getReleaseDate())
+                .build();
+    }
+}
---
+++ src/main/java/ru/yandex/practicum/filmorate/dto/user/UserMapper.java
@@ -0,0 +1,29 @@
+package ru.yandex.practicum.filmorate.dto.user;
+
+import ru.yandex.practicum.filmorate.user.User;
+
+import static ru.yandex.practicum.filmorate.util.Validators.isValidString;
+
+public class UserMapper {
+
+    public static User toEntity(UserCreateDto userCreateDto) {
+        return User.builder()
+                .name(isValidString(userCreateDto.getName()) ? userCreateDto.getName() : userCreateDto.getLogin())
+                .login(userCreateDto.getLogin())
+                .email(userCreateDto.getEmail())
+                .birthday(userCreateDto.getBirthday())
+                .build();
+    }
+
+    public static User toEntity(UserUpdateDto userUpdateDto) {
+        return User.builder()
+                .id(userUpdateDto.getId())
+                .email(userUpdateDto.getEmail().orElse(null))
+                .name(
+                        isValidString(userUpdateDto.getName().orElse(null)) ?
+                                userUpdateDto.getName().get() : userUpdateDto.getLogin())
+                .login(userUpdateDto.getLogin())
+                .birthday(userUpdateDto.getBirthday())
+                .build();
+    }
+}

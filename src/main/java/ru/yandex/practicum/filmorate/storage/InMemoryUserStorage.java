c74e866dd9b6aabb5f8a0f54b0cb7b6df46c2e2a
Petr <red19650226@yandex.ru>
2025-08-16 19:01:18 +0300

    style(cleanup): Cleanup

--- src/main/java/ru/yandex/practicum/filmorate/storage/InMemoryUserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/InMemoryUserStorage.java
@@ -1,7 +1,8 @@
 package ru.yandex.practicum.filmorate.storage;
 
 import java.lang.reflect.Field;
-import java.util.*;
+import java.util.Collection;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import jakarta.validation.ValidationException;

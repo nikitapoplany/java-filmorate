ec7dfe08ea687a2fc94f91c6a8d1f6942897519c
h0ttab <myemail@example.com>
2025-11-05 19:41:47 +0300

    feat: Add new validations

--- src/main/java/ru/yandex/practicum/filmorate/service/MpaService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/MpaService.java
@@ -6,8 +6,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.exception.LoggedException;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.Mpa;
 import ru.yandex.practicum.filmorate.storage.MpaDbStorage;
+import ru.yandex.practicum.filmorate.util.Validators;
 
 @Service
 public class MpaService {
@@ -24,6 +27,11 @@ public class MpaService {
     }
 
     public Mpa findById(Integer mpaId) {
+        if (!Validators.isValidMpa(mpaId)) {
+            LoggedException.throwNew(
+                    new NotFoundException(String.format("MPA id %d не найден.", mpaId)), getClass()
+            );
+        }
         return mpaStorage.findById(mpaId);
     }
 }
--- src/main/java/ru/yandex/practicum/filmorate/util/Validators.java
+++ src/main/java/ru/yandex/practicum/filmorate/util/Validators.java
@@ -3,10 +3,13 @@ package ru.yandex.practicum.filmorate.util;
 import java.time.LocalDate;
 
 import org.apache.commons.validator.routines.EmailValidator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 public class Validators {
-
     public static final int MAX_FILM_DESCRIPTION_LENGTH = 200;
+    @Autowired
+    private static JdbcTemplate jdbcTemplate;
 
     public static boolean isValidEmail(String email) {
         return EmailValidator.getInstance().isValid(email);
@@ -28,4 +31,35 @@ public class Validators {
         return releaseDate.isAfter(minDate);
     }
 
+    public static boolean isExistingLike(Integer filmId, Integer userId) {
+        String query = """
+                CASE
+                	WHEN (SELECT count(*) FROM "like" WHERE user_id = ? AND film_id = ?) > 0 THEN TRUE
+                	ELSE FALSE
+                END;
+                """;
+        return Boolean.TRUE.equals(jdbcTemplate.queryForObject(query, Boolean.class, userId, filmId));
+    }
+
+    public static boolean isValidMpa(Integer mpaId) {
+        String query = """
+                    SELECT
+                    CASE
+                    	WHEN ? IN (SELECT id FROM mpa) THEN TRUE
+                    	ELSE FALSE
+                    END;
+                """;
+        return Boolean.TRUE.equals(jdbcTemplate.queryForObject(query, Boolean.class, mpaId));
+    }
+
+    public static boolean isValidGenre(Integer genreId) {
+        String query = """
+                    SELECT
+                    CASE
+                    	WHEN ? IN (SELECT id FROM genre) THEN TRUE
+                    	ELSE FALSE
+                    END;
+                """;
+        return Boolean.TRUE.equals(jdbcTemplate.queryForObject(query, Boolean.class, genreId));
+    }
 }

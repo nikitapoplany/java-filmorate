b1706398deb38dea3aee72109d41178632024f28
h0ttab <myemail@example.com>
2025-11-05 22:29:20 +0300

    refactor: Separate common and db validations

---
+++ src/main/java/ru/yandex/practicum/filmorate/util/ValidatorsDb.java
@@ -0,0 +1,56 @@
+package ru.yandex.practicum.filmorate.util;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ValidatorsDb {
+    
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    public boolean isExistingLike(Integer filmId, Integer userId) {
+        String query = """
+                CASE
+                	WHEN (SELECT count(*) FROM "like" WHERE user_id = ? AND film_id = ?) > 0 THEN TRUE
+                	ELSE FALSE
+                END;
+                """;
+        return Boolean.TRUE.equals(jdbcTemplate.queryForObject(query, Boolean.class, userId, filmId));
+    }
+
+    public boolean isValidMpa(Integer mpaId) {
+        String query = """
+                    SELECT
+                    CASE
+                    	WHEN ? IN (SELECT id FROM mpa) THEN TRUE
+                    	ELSE FALSE
+                    END;
+                """;
+        return Boolean.TRUE.equals(jdbcTemplate.queryForObject(query, Boolean.class, mpaId));
+    }
+
+    public boolean isValidGenre(Integer genreId) {
+        String query = """
+                    SELECT
+                    CASE
+                    	WHEN ? IN (SELECT id FROM genre) THEN TRUE
+                    	ELSE FALSE
+                    END;
+                """;
+        return Boolean.TRUE.equals(jdbcTemplate.queryForObject(query, Boolean.class, genreId));
+    }
+
+    public boolean isValidFriend(Integer userIdA, Integer userIdB) {
+        String query = """
+                SELECT CASE
+                	WHEN (SELECT COUNT(*) FROM friends WHERE request_from_id = ? AND request_to_id = ?) > 0 THEN TRUE
+                	ELSE FALSE
+                END;
+                """;
+        Integer result = jdbcTemplate.queryForObject(query, Integer.class, userIdA, userIdB);
+
+        return result == 1;
+    }
+}

4f4e4eb9f379f92d0d3d47cd036413325735077b
Petr <red19650226@yandex.ru>
2025-11-08 19:57:42 +0300

    test: Add FilmStorage integration tests

---
+++ src/test/java/ru/yandex/practicum/filmorate/storage/FilmStorageTest.java
@@ -0,0 +1,143 @@
+package ru.yandex.practicum.filmorate.storage;
+
+import java.time.LocalDate;
+import java.util.*;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.context.annotation.Import;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
+import ru.yandex.practicum.filmorate.mapper.*;
+import ru.yandex.practicum.filmorate.model.*;
+import ru.yandex.practicum.filmorate.service.*;
+import ru.yandex.practicum.filmorate.util.ValidatorsDb;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@JdbcTest
+@AutoConfigureTestDatabase
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
+@Import({FilmDbStorage.class,
+        FilmRowMapper.class,
+        MpaService.class,
+        MpaDbStorage.class,
+        ValidatorsDb.class,
+        MpaRowMapper.class,
+        GenreService.class,
+        GenreDbStorage.class,
+        GenreRowMapper.class,
+        FilmMapper.class,
+        LikeDbStorage.class,
+        LikeService.class})
+public class FilmStorageTest {
+    private final FilmDbStorage storage;
+
+    private void assertFilm(Film film, Integer id, String name, String description, LocalDate releaseDate,
+                            Integer duration, Mpa mpa, Set<Integer> likes, List<Genre> genres) {
+        assertThat(film)
+                .hasFieldOrPropertyWithValue("id", id)
+                .hasFieldOrPropertyWithValue("name", name)
+                .hasFieldOrPropertyWithValue("description", description)
+                .hasFieldOrPropertyWithValue("releaseDate", releaseDate)
+                .hasFieldOrPropertyWithValue("duration", duration)
+                .hasFieldOrPropertyWithValue("mpa", mpa)
+                .hasFieldOrPropertyWithValue("likes", likes)
+                .hasFieldOrPropertyWithValue("genres", genres);
+    }
+
+    @Test
+    public void testFindById() {
+        Film film = storage.findById(3);
+
+        assertFilm(film, 3, "Шрек", "История про зеленого огра и его приключения.",
+                LocalDate.of(2001,5, 18), 90, Mpa.builder().id(2).name("PG").build(),
+                Set.of(2), List.of(
+                        Genre.builder().id(1).name("Комедия").build(),
+                        Genre.builder().id(3).name("Мультфильм").build())
+        );
+    }
+
+    @Test
+    public void testFindAll() {
+        List<Film> films = storage.findAll();
+
+        assertThat(films)
+                .hasSize(3)
+                .extracting(Film::getId)
+                .containsExactlyInAnyOrder(1, 2, 3);
+
+        films.forEach(film -> {
+            if (film.getId() == 3) {
+                assertFilm(film, 3, "Шрек", "История про зеленого огра и его приключения.",
+                        LocalDate.of(2001,5, 18), 90, Mpa.builder().id(2).name("PG").build(),
+                        Set.of(2), List.of(
+                                Genre.builder().id(1).name("Комедия").build(),
+                                Genre.builder().id(3).name("Мультфильм").build())
+                );
+            }
+        });
+    }
+
+    @Test
+    public void testFilmCreate() {
+        Film film = Film.builder()
+                .name("Оно")
+                .description("Леденящее воплощение ужаса притаилось в тени и повсюду")
+                .releaseDate(LocalDate.of(1990, 11, 18)).duration(192)
+                .mpa(Mpa.builder().id(2).name("PG").build())
+                .genres(new ArrayList<>(List.of(Genre.builder().id(2).name("Драма").build())))
+                .build();
+        Film createdFilm = storage.create(film);
+        Film createdFilmFromDb = storage.findById(createdFilm.getId());
+
+        assertThat(film)
+                .isEqualTo(createdFilm)
+                .isEqualTo(createdFilmFromDb);
+    }
+
+    @Test
+    public void testFilmUpdate() {
+        Film originalFilm = storage.findById(1);
+        Film update = originalFilm.toBuilder()
+                .name("UPDATED NAME")
+                .mpa(Mpa.builder().id(5).name("NC-17").build())
+                .genres(new ArrayList<>(List.of(
+                        Genre.builder().id(2).name("Драма").build(),
+                        Genre.builder().id(6).name("Боевик").build(),
+                        Genre.builder().id(4).name("Триллер").build()))
+                )
+                .build();
+
+        assertThat(originalFilm)
+                .hasFieldOrPropertyWithValue("name", "Криминальное чтиво")
+                .hasFieldOrPropertyWithValue("mpa", Mpa.builder().id(4).name("R").build());
+        assertThat(originalFilm.getGenres()).extracting(Genre::getId).containsExactlyInAnyOrder(2, 6);
+
+        storage.update(update);
+
+        Film updatedFilm = storage.findById(1);
+
+        assertThat(updatedFilm)
+                .hasFieldOrPropertyWithValue("name", "UPDATED NAME")
+                .hasFieldOrPropertyWithValue("mpa", Mpa.builder().id(5).name("NC-17").build());
+        assertThat(updatedFilm.getGenres()).extracting(Genre::getId).containsExactlyInAnyOrder(2, 6, 4);
+    }
+
+    @Test
+    public void testFilmDelete() {
+        Assertions.assertDoesNotThrow(() -> storage.findById(1));
+        storage.delete(1);
+        Assertions.assertThrows(NotFoundException.class, () -> storage.findById(1));
+    }
+
+    @Test
+    public void testFindTopLiked() {
+        List<Film> films = storage.findTopLiked(3);
+        assertThat(films).extracting(Film::getId).containsExactly(3, 1, 2);
+    }
+}
